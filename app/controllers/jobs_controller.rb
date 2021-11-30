d1bdcc4739d2f472f0716773cb4f3e999b0ea6ef
Ben LaFon <benjamin.lafon@outlook.com>
2021-11-30 17:59:00 +0800

    idk some more stuff okay

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,14 @@
 class JobsController < ApplicationController
-
+  before_action :set_job, only: [:show]
   def show
   end
 
   def index
   end
-  
 
+  private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
 end
