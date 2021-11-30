7729d47ab8019f9ebde3ec8e763aa68a4b4b6797
Gary Shih <garyjshih@gmail.com>
2021-11-30 16:53:13 +0800

    tailwind for dashboard profile jobs1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :resource, only: [ :show, :index ]
-  resources :job, only: [ :show, :index ]
+  resources :resources, only: [ :show, :index ]
+  resources :jobs, only: [ :show, :index ]
   get 'dashboard', to: 'pages#dashboard'
   get 'profile', to: 'pages#profile'
 
