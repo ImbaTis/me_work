3a95427ea9073b3b93ecbae1ed59a7ee3f24d4e7
Ben LaFon <benjamin.lafon@outlook.com>
2021-12-01 14:34:30 +0800

    added all the stuff to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,34 @@ end
 end
 
 10.times do
-  user = User.new(email: Faker::Internet.free_email, password: Devise.friendly_token)
+  user = User.new(
+    email: Faker::Internet.free_email,
+    password: Devise.friendly_token,
+    pic_url: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    location: "ShangHai",
+    languages: "English, Chinese, Spanish",
+    code: "Ruby, Python, HTML, CSS",
+    slogan: "Carpe Diem",
+    quote: "I like all the birds",
+    passions: "underwater basket weaving",
+    hobbies: "magnets",
+    story: "Born, lived... TBD",
+    work_style: 'Working with the crazies',
+    causes: "the penguins",
+    why: "To leave a better world",
+    hours: "50/50 WFH",
+    commute: "30 mins max",
+    skills: "Ninja-ing, filing taxes",
+    contribute: "more like what CANT I contribute",
+    comp: "whatever Warren Buffet makes... plus 5",
+    teammates: "easy going, hard workers",
+    boss: "a boss who leads from the front and has his teams best interest at heart",
+    org_structure: "pyramid shaped with multiple levels",
+    size: " Publicaly traded mega corporation",
+    work_structure: "uhm yes",
+    amenities: "Gym, meditation space, open bar",
+    travel: "As little travel as possible"
+  )
   user.save!
   puts "user #{user.id} created"
   rand(5..15).times do
@@ -46,8 +73,63 @@ end
   end
 end
 
+puts "now for the test acct"
 
+user = User.new(
+    email: "test@test.com",
+    password: "123456",
+    pic_url: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    location: "ShangHai",
+    languages: "English, Chinese, Spanish",
+    code: "Ruby, Python, HTML, CSS",
+    slogan: "Carpe Diem",
+    quote: "I like all the birds",
+    passions: "underwater basket weaving",
+    hobbies: "magnets",
+    story: "Born, lived... TBD",
+    work_style: 'Working with the crazies',
+    causes: "the penguins",
+    why: "To leave a better world",
+    hours: "50/50 WFH",
+    commute: "30 mins max",
+    skills: "Ninja-ing, filing taxes",
+    contribute: "more like what CANT I contribute",
+    comp: "whatever Warren Buffet makes... plus 5",
+    teammates: "easy going, hard workers",
+    boss: "a boss who leads from the front and has his teams best interest at heart",
+    org_structure: "pyramid shaped with multiple levels",
+    size: " Publicaly traded mega corporation",
+    work_structure: "uhm yes",
+    amenities: "Gym, meditation space, open bar",
+    travel: "As little travel as possible"
+  )
+  user.save!
+  puts "user #{user.id} created"
+  rand(5..15).times do
+    fav = Favorite.new(
+      user: user,
+      record: Job.all.sample
+    )
+    unless fav.save
+      puts "fav error #{fav.errors.full_messages}"
+    end
+    puts "fav #{fav.id} created"
+  end
+  rand(5..15).times do
+    fav = Favorite.new(
+      user: user,
+      record: Resource.all.sample
+    )
+    unless fav.save
+      puts "fav error #{fav.errors.full_messages}"
+    end
+    puts "fav #{fav.id} created"
+  end
 
-
-
-# 10.times do Resource.create(name: )
+  rand(1..10).times do
+    res = TestResult.new(name: Faker::GreekPhilosophers.name, content: Faker::ChuckNorris.fact, user: user)
+    unless res.save
+        puts "fav error #{res.errors.full_messages}"
+    end
+    puts "fav #{res.id} created"
+  end
